3904631dcebe79bfa34b253a89bac8557904e697
Bruno Campagnol <bruno_unky@hotmail.com>
2024-04-04 19:18:20 -0300

    Obs

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ import json
 
 def main():
 
+    print("Consulta de DDD para saber quais são as cidades que compoem o mesmo")
+    print("DDD usual não precisa por o 0 inicial")
+
     ddd = input("Informe o DDD: ")
 
     URL = f"https://brasilapi.com.br/api/ddd/v1/{ddd}"
