03211eb041fd438e3697d0ead8f04e6bff6fe89d
Bruno Campagnol <bruno_unky@hotmail.com>
2024-04-04 19:11:03 -0300

    Correcao

--- index.py
+++ index.py
@@ -2,11 +2,14 @@ import requests
 import json
 
 def main():
-    URL = "https://brasilapi.com.br/api/ddd/v1/15"
+
+    ddd = input("Informe o DDD: ")
+
+    URL = f"https://brasilapi.com.br/api/ddd/v1/{ddd}"
     req = requests.get(URL)
     dados_recebidos = req.json()
 
-    print(json.loads(req.content))
+    print(json.loads(req.text))
 
 if __name__ == "__main__":
     main()
